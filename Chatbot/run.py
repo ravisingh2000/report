08b6d1745b1d02127dcde9ac3df6f27c4946d228
ravisingh2000 <61704011+ravisingh2000@users.noreply.github.com>
2020-04-10 12:45:28 +0530

    Add files via upload

---
+++ Chatbot/run.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+os.remove("db.sqlite3")
+english_bot = ChatBot('Bot')
+english_bot.set_trainer(ListTrainer)
+for file in os.listdir('data'):
+        convData = open('data/' + file).readlines()
+        english_bot.train(convData)
+
+app = Flask(__name__)
+
+english_bot = ChatBot('Bot',
+             storage_adapter='chatterbot.storage.SQLStorageAdapter',
+             logic_adapters=[
+   {
+       'import_path': 'chatterbot.logic.BestMatch'
+   },
+   
+],
+trainer='chatterbot.trainers.ListTrainer')
+english_bot.set_trainer(ListTrainer)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    response = str(english_bot.get_response(userText))
+    return response
+
+if __name__ == "__main__":
+    app.run()
